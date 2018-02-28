96a783507d331a5ba9de0c785eadc6331ec2a494
Aditya Rao <adityasrao1000@gmail.com>
2018-02-28 23:26:55 +0530

    IPaddress method moved to separate class

--- src/multimedia/image/DownloadImage.java
+++ src/multimedia/image/DownloadImage.java
@@ -16,6 +16,7 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import static multimedia.session.IPAddress.getClientIpAddr;
 
 @Controller
 @RequestMapping("/download")
@@ -61,41 +62,4 @@ public class DownloadImage {
 		    db.close(ps, rs, con);
 	}	
   } 
-	public static String getClientIpAddr(HttpServletRequest request) {  
-	    String ip = request.getHeader("X-Forwarded-For");  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("WL-Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_VIA");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("REMOTE_ADDR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getRemoteAddr();  
-	    }  
-	    return ip;  
-	}
 }

83c2177cc4c9e94b7b2f72a7ab9f27351b483750
Aditya Rao <adityasrao1000@gmail.com>
2018-02-28 23:26:02 +0530

    bug fix

--- src/multimedia/session/SessionCheckFilter.java
+++ src/multimedia/session/SessionCheckFilter.java
@@ -7,22 +7,22 @@ import javax.servlet.http.*;
 
 public class SessionCheckFilter implements Filter{  
   
-public void init(FilterConfig arg0) throws ServletException {}  
-      
-public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {  
-          
-	HttpServletRequest request = (HttpServletRequest) req;
-    HttpSession session = request.getSession(false);
-    HttpServletResponse response = (HttpServletResponse) resp;
-    
-    if(session ==null || session.getAttribute("email")==null){  
-        
-        response.sendRedirect("/multimedia/login");  
-                
-    }  
-    else{  
-    	chain.doFilter(req, resp);//sends request to next resource 
-    }  
-}  
+	public void init(FilterConfig arg0) throws ServletException {}  
+	      
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {  
+	          
+		HttpServletRequest request = (HttpServletRequest) req;
+	    HttpSession session = request.getSession(false);
+	    HttpServletResponse response = (HttpServletResponse) resp;
+	    
+	    if(session ==null || session.getAttribute("email")==null){  
+	        
+	        response.sendRedirect("login");  
+	                
+	    }  
+	    else{  
+	    	chain.doFilter(req, resp);//sends request to next resource 
+	    }  
+	}  
     public void destroy() {}
 }  
